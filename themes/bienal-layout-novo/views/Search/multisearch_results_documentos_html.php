c4ea8bcd2961cc3b254408810b586e14001867bb
paramo <contato.faccti@gmail.com>
2025-03-26 20:29:36 +0100

    FOBDSOP-179 Corrigir layout do resultado de busca

--- themes/bienal-layout-novo/views/Search/multisearch_results_documentos_html.php
+++ themes/bienal-layout-novo/views/Search/multisearch_results_documentos_html.php
@@ -25,51 +25,48 @@
  *
  * ----------------------------------------------------------------------
  */
- 
-	$results = $this->getVar('result');
-	$block_info = $this->getVar('blockInfo');
-	$vn_start		 	= (int)$this->getVar('start');
-	$itemsPerPage 	= (int)$this->getVar('itemsPerPage');
-	$vs_search 			= (string)$this->getVar('search');
-	$vn_init_with_start	= (int)$this->getVar('initializeWithStart');
+
+$results = $this->getVar('result');
+$block_info = $this->getVar('blockInfo');
+$vn_start             = (int)$this->getVar('start');
+$itemsPerPage     = (int)$this->getVar('itemsPerPage');
+$vs_search             = (string)$this->getVar('search');
+$vn_init_with_start    = (int)$this->getVar('initializeWithStart');
 
 ?>
 
-<?php if ( $results->numHits() > 0) : ?>
+<?php if ($results->numHits() > 0) : ?>
 
     <div class="sidebar">
         <div class="sidebar-header">
-            <?= _t($block_info['displayName']); ?> 
+            <?= var_dump($block_info['displayName']) ?>
+            <?= _t($block_info['displayName']); ?>
             <br>
             <span class="sidebar-header-results-counter"><?= $results->numHits() . " " . _t("resultados") ?></span>
         </div>
-                
+
         <div class="sidebar-items">
-        <?php
+            <?php
             $index = 0;
-            while($results->nextHit()) 
-            {
+            while ($results->nextHit()) {
             ?>
-            
+
                 <div class="sidebar-item">
-                    <a href="<?php echo $this->request->getBaseUrlPath(); ?>/index.php/Detail/documento/<?=$results->getPrimaryKey()?>">
-                        <strong><?php echo $results->get('ca_objects.type_id', array( 'convertCodesToDisplayText' => true ));?></strong> <?php echo $results->get('ca_objects.hierarchy.preferred_labels', array( 'delimiter' => ' -> '));?>
+                    <a href="<?php echo $this->request->getBaseUrlPath(); ?>/index.php/Detail/documento/<?= $results->getPrimaryKey() ?>">
+                        <strong><?php echo $results->get('ca_objects.type_id', array('convertCodesToDisplayText' => true)); ?></strong> <?php echo $results->get('ca_objects.hierarchy.preferred_labels', array('delimiter' => ' -> ')); ?>
                     </a>
                 </div>
-                
+
             <?php
                 $index++;
-                if ( $index == $itemsPerPage || $index >= $itemsPerPage ) {break;} 
+                if ($index == $itemsPerPage || $index >= $itemsPerPage) {
+                    break;
+                }
             }
-        ?>
+            ?>
         </div>
 
-        <div class="all-results-button">
-            <button class="select-btn">
-                <img src="<?= $this->request->getBaseUrlPath() ?>/themes/bienal-layout-novo/assets/svg/magnifying-glass.svg" />
-                <?php print caNavLink($this->request, 'Veja todos os resultados', '', '', 'Search', '{{{block}}}', array('search' => $vs_search)); ?>
-            </button>
-        </div>
-    </div>    
-            
+        <?php print caNavLink($this->request, 'Veja todos os resultados', 'all-results-button', '', 'Search', '{{{block}}}', array('search' => $vs_search)); ?>
+    </div>
+
 <?php endif; ?>
